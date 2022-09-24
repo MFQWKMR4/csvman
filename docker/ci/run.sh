88733d8ad26981f9ac1362d2968fb29864a3d233
MFQWKMR4 <kenfan.fun@icloud.com>
2022-09-24 13:38:33 +0900

    enable native compile and sbt test

--- docker/ci/run.sh
+++ docker/ci/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-docker run --rm -it --mount type=bind,src="$(pwd)/../../",dst="/root/csvman_test" --name csvman_test csvman_test:1.0 /bin/bash -c "sbt test" "exit"
+docker run --rm \
+    -it \
+    --mount type=bind,src="$(pwd)/../../",dst="/root/csvman_test" \
+    --name csvman_test csvman_test:1.1 \
+    /bin/bash -c 'source "/root/.sdkman/bin/sdkman-init.sh" && cd /root/csvman_test && sbt test'
